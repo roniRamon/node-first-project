f56eb408c643478c7e68999ba247769f3c768fbc
RoniRamon <roniramon260@gmail.com>
2018-07-08 13:54:08 -0700

    create server http

--- animal_fun.js
+++ animal_fun.js
@@ -1,84 +1,91 @@
-const fs = require('fs')
-const http = require('http')
-const qs = require('querystring')
-const cache = {}
+const fs = require('fs');
+const http = require('http');
+const qs = require('querystring');
+const cache = {};
 
-// fs.readFile('./animals.txt', 'utf-8', (err, data) => {
-//   if (err) {
-//     console.log(err)
-//     return
-//   }
-//   console.log(data)
-// })
+fs.readFile('./animals.txt', 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(data);
+});
 
-// fs.writeFile('./example.txt', 'I will be written to example.txt', err => {
-//   if (err) {
-//     console.log(err)
-//     return
-//   }
-//   console.log('file successfully written')
-// })
+fs.writeFile('./example.txt', 'I will be written to example.txt', err => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log('file successfully written');
+});
 
-// function selectAnimals(animalString, animalLetter) {
-//   return animalString
-//     .split('\n')
-//     .filter(animal => animal.startsWith(animalLetter))
-//     .join('\n')
-// }
+function selectAnimals(animalString, animalLetter) {
+  return animalString
+    .split('\n')
+    .filter(animal => animal.startsWith(animalLetter))
+    .join('\n');
+}
 
-// const animalLetter = process.argv[2].toUpperCase()
+const animalLetter = process.argv[2].toUpperCase();
 
-// fs.readFile('./animals.txt', 'utf-8', (err, data) => {
-//   if (err) {
-//     console.log(err)
-//     return
-//   }
-//   const animals = selectAnimals(data, animalLetter)
+fs.readFile('./animals.txt', 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  const animals = selectAnimals(data, animalLetter);
 
-//   fs.writeFile(`${animalLetter}_animals.txt`, animals, err => {
-//     if (err) {
-//       console.log(err)
-//       return
-//     }
-//     console.log(`successfully created ${animalLetter}_animals.txt`)
-//   })
-// })
+  fs.writeFile(`${animalLetter}_animals.txt`, animals, error => {
+    if (error) {
+      console.log(error);
+      return;
+    }
+    console.log(`successfully created ${animalLetter}_animals.txt`);
+  });
+ });
 
-// const animalServer = http.createServer((req, res) => {
-//   const query = req.url.split('?')[1]
-//   if (query !== undefined) {
-//     const animalLetter = qs.parse(query).letter.toUpperCase()
+ // const server = http.createServer((req, res) => {
+ //   res.write('hello world');
+ //   res.end();
+ // });
+ //
+ // server.listen(8000, () => console.log("I'm listening on port 8000!"));
 
-//     if (cache[animalLetter] !== undefined) {
-//       res.end(cache[animalLetter])
-//     }
+const animalServer = http.createServer((req, res) => {
+  const query = req.url.split('?')[1];
+  if (query !== undefined) {
+    const animalLetter2 = qs.parse(query).letter.toUpperCase();
 
-//     if (animalLetter !== undefined) {
-//       fs.readFile('./animals.txt', 'utf-8', (err, data) => {
-//         if (err) {
-//           console.log(err)
-//           res.end('IT WENT POORLY')
-//           return
-//         }
-//         const animals = selectAnimals(data, animalLetter)
-//         cache[animalLetter] = animals
-//         res.end(animals)
-//       })
-//     }
-//   } else {
-//     if (cache['animals'] !== undefined) {
-//       res.end(cache['animals'])
-//     }
-//     fs.readFile('./animals.txt', 'utf-8', (err, data) => {
-//       if (err) {
-//         console.log(err)
-//         res.end('IT WENT POORLY')
-//         return
-//       }
-//       cache['animals'] = data
-//       res.end(data)
-//     })
-//   }
-// })
+    if (cache[animalLetter2] !== undefined) {
+      res.end(cache[animalLetter2]);
+    }
 
-// animalServer.listen(8000, () => console.log("I'm listening on port 8000"))
+    if (animalLetter2 !== undefined) {
+      fs.readFile('./animals.txt', 'utf-8', (err, data) => {
+        if (err) {
+          console.log(err);
+          res.end('IT WENT POORLY');
+          return;
+        }
+        const animals = selectAnimals(data, animalLetter2);
+        cache[animalLetter2] = animals;
+        res.end(animals);
+      });
+    }
+  } else {
+    if (cache['animals'] !== undefined) {
+      res.end(cache['animals']);
+    }
+    fs.readFile('./animals.txt', 'utf-8', (err, data) => {
+      if (err) {
+        console.log(err);
+        res.end('IT WENT POORLY');
+        return;
+      }
+      cache['animals'] = data;
+      res.end(data);
+    });
+  }
+});
+
+animalServer.listen(8000, () => console.log("I'm listening on port 8000"));
--- github_grabber.js
+++ github_grabber.js
@@ -1,7 +1,7 @@
-const fs = require('fs')
-const qs = require('qs')
-const http = require('http')
-const https = require('https')
+const fs = require('fs');
+const qs = require('qs');
+const http = require('http');
+const https = require('https');
 
 function buildOptionsObj (username) {
   return {
@@ -10,32 +10,32 @@ function buildOptionsObj (username) {
     headers: {
       'User-Agent': 'github-grabber'
     }
-  }
+  };
 }
 
 const githubServer = http.createServer((req, res) => {
   if (req.method === 'POST') {
-    let requestBody = ''
+    let requestBody = '';
     req.on('data', chunk => {
-      requestBody += chunk
-    })
+      requestBody += chunk;
+    });
     req.on('end', () => {
-      const username = qs.parse(requestBody).username
-      const ws = fs.createWriteStream(`./${username}_starred_repos.txt`)
-      const opts = buildOptionsObj(username)
+      const username = qs.parse(requestBody).username;
+      const ws = fs.createWriteStream(`./${username}_starred_repos.txt`);
+      const opts = buildOptionsObj(username);
       https.get(opts, (dataStream) => {
-        let repoData = ''
-        dataStream.on('data', chunk => { repoData += chunk })
+        let repoData = '';
+        dataStream.on('data', chunk => { repoData += chunk; });
         dataStream.on('end', () => {
           const repos = JSON.parse(repoData).map(repo => {
-            return `Repo: ${repo.name}. Stars: ${repo.stargazers_count}.`
-          }).join('\n')
-          ws.write(repos)
-          res.end(repos)
-        })
-      })
-    })
+            return `Repo: ${repo.name}. Stars: ${repo.stargazers_count}.`;
+          }).join('\n');
+          ws.write(repos);
+          res.end(repos);
+        });
+      });
+    });
   }
-})
+});
 
-githubServer.listen(8080, () => console.log('Listening on 8080'))
+githubServer.listen(8080, () => console.log('Listening on 8080'));
